bfad2d66946ef042c52c2b5f5f9620f707959c3a
Jordan Else <jordan.else@hotmail.co.uk>
2024-08-08 20:07:24 +0100

    Add solution for first test in repeatString assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let text = string;
+    let returnText = text;
+    for(i = 0; i < num-1; i++){             //iterate through num arguement -1 times, as
+        returnText = returnText + text;     //first is already assigned
+    };
+    return returnText
 };
 
 // Do not edit below this line
