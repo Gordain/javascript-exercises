0f1ee7da8fc6c179eb603784fdc9b11c343bf8d6
Jordan Else <jordan.else@hotmail.co.uk>
2024-08-17 18:12:28 +0100

    Add condition for when num input is negative

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,6 +3,8 @@ const repeatString = function(string, num) {
     let returnText = text;
     if(num == 0){
         return "";
+    }else if(num<0){
+        return "ERROR";
     }
     for(i = 0; i < num-1; i++){             //iterate through num arguement -1 times, as
         returnText = returnText + text;     //first is already assigned
