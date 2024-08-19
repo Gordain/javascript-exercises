1ab2ae6bce6dc7880a5d82111f9282aa15acd296
Jordan Else <jordan.else@hotmail.co.uk>
2024-08-19 18:54:08 +0100

    Add initial solution for first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let wordToBeReversed = input;
+    let stringArray = wordToBeReversed.split("");
+    stringArray.reverse();
+    let reversedWord = stringArray.toString();
+    return reversedWord
 };
 
 // Do not edit below this line
