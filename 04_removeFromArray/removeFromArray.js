5213bedfe5237aa6a9e8387e2150d36ffec6612c
Jordan Else <22601206+Gordain@users.noreply.github.com>
2024-08-22 20:42:20 +0100

    Add nested loops for iteration through every possible given array index, passes first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayArgs, ...removeArgs) {    //rest argument to allow undefined number of items to be removed
+    let givenArray = arrayArgs;
+    let itemsToRemove = removeArgs;
+    for (let i = 0; i < givenArray.length; i++){
+        for(let j = 0; j < itemsToRemove.length; j++){
+            if(givenArray[i]==itemsToRemove[j]){
+                givenArray.splice(i,1);
+            }
+        }
+    }
+    return givenArray;
 };
 
 // Do not edit below this line
