d4be1397cc242492d60b5ee3059264c0ab193327
Jordan Else <22601206+Gordain@users.noreply.github.com>
2024-08-23 20:52:16 +0100

    Change loop to decrement as the original array length changes so was causing issues in multiple values not being removed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,11 @@
 const removeFromArray = function(arrayArgs, ...removeArgs) {    //rest argument to allow undefined number of items to be removed
     let givenArray = arrayArgs;
     let itemsToRemove = removeArgs;
-    for (let i = 0; i < givenArray.length; i++){
+    console.log(itemsToRemove);
+    for (let i = givenArray.length -1; i>=0; i--){          //as the length of the array changes from removed items need to loop backwards
         for(let j = 0; j < itemsToRemove.length; j++){
             if(givenArray[i]==itemsToRemove[j]){
-                givenArray.splice(i,1);
+                givenArray.splice(i,1);                 //nested loop to iterate through every possible given item to remove
             }
         }
     }
