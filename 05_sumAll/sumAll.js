549248f0a2e88d42882895d577b71e8f7031229f
Jordan Else <22601206+Gordain@users.noreply.github.com>
2024-08-24 20:22:38 +0100

    Add inital conditions and code to sum numbers inbetween two gievn numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let firstNumber = a;
+    let secondNumber = b;
+    let sumOfNumbers = 0;
+    if(firstNumber < 0 || secondNumber < 0 || !Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)){
+        return "ERROR";
+    }else if(firstNumber > secondNumber){
+        for(let i = secondNumber; secondNumber <= firstNumber; i++){
+            sumOfNumbers += i;
+        }
+        return sumOfNumbers;
+    }else{
+        for(let i = firstNumber; firstNumber <= secondNumber; i++){
+            sumOfNumbers += i;
+        }
+        return sumOfNumbers;
+    }
 
 };
 
