5db1130e4f81526f09f122af894bdc6410ce54e7
Jordan Else <22601206+Gordain@users.noreply.github.com>
2024-08-26 21:21:18 +0100

    Intial code for elimination of years that are not leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    let year = givenYear;
+    if(Number.isInteger(year/4) && Number.isInteger(year/400)){     // compares if given number is divisble by 4 and 400, for leap year
+        return true;
+    }else if(Number.isInteger(year/4) && Number.isInteger(year/100)){   //eliminates years divisible by both 4 and 100
+        return false;
+    }else if(Number.isInteger(year/4)){     //last check for those years that remain, divisible by 4
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
