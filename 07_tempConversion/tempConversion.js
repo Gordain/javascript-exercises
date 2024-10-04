f9a7ac7e2227b6fe0c1040921191e108379c59af
Jordan Else <22601206+Gordain@users.noreply.github.com>
2024-10-04 18:04:43 +0100

    Add Convertions for both celsius and fahrenheit, with rounding to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(givenFahrenheit) {
+  let convertedCelsius = (givenFahrenheit - 32)*(5/9);
+  let roundedCelsius = Math.round(convertedCelsius*10)/10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(givenCelsius) {
+  let convertedFahrenheit = (givenCelsius*(9/5))+32;
+  let roundedFahrenheit = Math.round(convertedFahrenheit*10)/10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
